a7790e92c694d72b51a582f7503b7f3b22d7114d
Tommy Marshall <tommy.marshall@viget.com>
2014-05-29 11:28:17 -0400

    Adds basic color logic. Gets HEX fields displaying properly.

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
 
-// console.log("id: " + id);
-// console.log("sColor: " + sColor);
-// console.log(elem);
-
-
-
-
-
 var COLOR = {
 	rgbToHex: function (R,G,B) {
 		var toHex =  function (n) {
@@ -25,29 +17,50 @@ var COLOR = {
 	},
 
 	vars: function() {
-		this.$body = $('body');
+		this.$body    = $('body');
 		this.$options = $('.color-picker-option');
+		this.$submit  = $('.calculate');
 	},
 
 	binds: function() {
 		var self = this;
 
-		this.$options.on('blur', this.updateColor.bind(this));
+		this.$options.on('blur focus', this.updateColor.bind(this));
+		this.$submit.on('click', this.updateOverlayOpacity.bind(this));
 	},
 
 	updateColor: function(e) {
-		var $item = $(e.currentTarget);
-		var className = $item.attr('class');
-		var targetClassName = '.'+className.substr(className.lastIndexOf(" ") + 1);
-		var $targets = $(targetClassName);
+		var $item            = $(e.currentTarget);
+		var className        = $item.attr('class');
+		var targetClassName  = '.'+className.substr(className.lastIndexOf(" ") + 1);
+		var $targets          = $(targetClassName);
 		var $closestColorBox = $targets.eq(0).parents('.form-group').find('.colorblock');
-		var color = this.rgbToHex($targets.eq(0).val(), $targets.eq(1).val(), $targets.eq(2).val());
+		var color;
 
-		// Assign color values to other inputs
-		$targets.val($item.val());
+		if ($targets.parents('#rgb').length > 0) {
+			color = this.rgbToHex($targets.eq(0).val(), $targets.eq(1).val(), $targets.eq(2).val());
 
-		// Assign color to closest color box
+			// Assign color values to other inputs
+			$targets.val($item.val());
+		} else {
+			color = $targets.val();
+		}
+
 		$closestColorBox.css('background-color', '#'+color);
+	},
+
+	updateOverlayOpacity: function(e) {
+		e.preventDefault();
+
+		var $final      = $('#rgb-opacity');
+		var $background = $('#background-color--r');
+		var $overlay    = $('#overlay-color--r');
+		var $target     = $('#target-color--r');
+
+		var result = ($target.val() - $background.val()) / ($overlay.val() - $background.val());
+		console.log('COLOR!', result);
+
+		$final.val(result * 100);
 	}
 
 };
