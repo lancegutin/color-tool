481b006c6ef7d815ceec8d2ff0cfd6777ea6aab7
Tommy Marshall <tommy.marshall@viget.com>
2014-06-19 11:32:00 -0400

    Update color demo option. Set all values to same result.

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 
 var COLOR = {
+	showError: function() {
+		this.$error.fadeIn();
+	},
+
+	hideError: function() {
+		this.$error.fadeOut();
+	},
+
 	rgbToHex: function (R,G,B) {
 		if (!R && !G && !B) return;
 
@@ -16,36 +24,42 @@ var COLOR = {
 	setup: function() {
 		this.vars();
 		this.binds();
+
+		this.$error.hide();
 	},
 
 	vars: function() {
 		this.$body    = $('body');
+		this.$blocks  = $('.colorblock');
 		this.$options = $('.color-picker-option');
 		this.$submit  = $('.calculate');
 		this.$reset   = $('.reset');
 		this.$form    = $('form');
+		this.$error   = $('.error');
 	},
 
 	binds: function() {
 		this.$options.on('blur focus', this.updateColor.bind(this));
-		this.$submit.on('click', this.updateOverlayOpacity.bind(this));
+		this.$blocks.on('change', function(){alert('awesome');});
+		this.$submit.on('click', this.calculate.bind(this));
 		this.$reset.on('click', this.reset.bind(this));
 	},
 
 	reset: function() {
+		this.$error.fadeOut();
 		this.$form.each(function(){
 			this.reset();
 		});
-		$('.colorblock').css('background-color', '#fff');
+		this.$blocks.css('background-color', '#fff');
 	},
 
 	updateColor: function(e) {
-		var $item            = $(e.currentTarget);
+		var $item = $(e.currentTarget);
 
 		if ($item.val() != '') {
 			var className        = $item.attr('class');
-			var targetClassName  = '.'+className.substr(className.lastIndexOf(" ") + 1);
-			var $targets          = $(targetClassName);
+			var targetClassName  = '.' + className.substr(className.lastIndexOf(" ") + 1);
+			var $targets         = $(targetClassName);
 			var $closestColorBox = $targets.eq(0).parents('.form-group').find('.colorblock');
 			var color;
 
@@ -61,12 +75,12 @@ var COLOR = {
 		}
 	},
 
-	updateOverlayOpacity: function(e) {
+	calculate: function(e) {
 		e.preventDefault();
 
-		var $row1 = $('#background-color--r');
-		var $row2 = $('#target-color--r');
-		var $row3 = $('#overlay-color--r');
+		var $row1 = $('#background-color--r').parents('.form-group').find('.color-picker-option');
+		var $row2 = $('#target-color--r').parents('.form-group').find('.color-picker-option');
+		var $row3 = $('#overlay-color--r').parents('.form-group').find('.color-picker-option');
 		var $row4 = $('#rgb-opacity');
 		var result;
 
@@ -74,13 +88,27 @@ var COLOR = {
 			$row4.val(100*(($row2.val() - $row1.val()) / ($row3.val() - $row1.val())));
 		}
 		else if ($row1.val() && $row2.val() && $row4.val()) {
-			$row3.val(($row2.val() - (1 - ($row4.val()/100)) * $row1.val()) / ($row4.val()/100));
+			$row3.val(Math.round(($row2.val() - (1 - ($row4.val()/100)) * $row1.val()) / ($row4.val()/100)));
+			$row3.trigger('blur');
 		}
 		else if ($row1.val() && $row3.val() && $row4.val()) {
-			$row2.val((($row4.val()/100) * $row3.val()) + (1 - ($row4.val()/100)) * $row1.val());
+			$row2.val(Math.round((($row4.val()/100) * $row3.val()) + (1 - ($row4.val()/100)) * $row1.val()));
+			$row2.trigger('blur');
 		}
 		else if ($row2.val() && $row3.val() && $row4.val()) {
-			$row1.val(($row2.val() - ($row4.val()/100) * $row3.val()) / (1 - ($row4.val()/100)));
+			$row1.val(Math.round(($row2.val() - ($row4.val()/100) * $row3.val()) / (1 - ($row4.val()/100))));
+			$row1.trigger('blur');
+		}
+
+		if (
+			($row1.val() > 255 || $row1.val() < 0) ||
+			($row2.val() > 255 || $row2.val() < 0) ||
+			($row3.val() > 255 || $row3.val() < 0) ||
+			($row4.val() > 100 || $row4.val() < 0)
+		) {
+			this.showError();
+		} else {
+			this.hideError();
 		}
 	}
 
